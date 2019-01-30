164053a13d0477a3b6d1bfe8aca751eb582db716
Mariusz <mariusz.kkk@poczta.fm>
2019-01-30 22:20:31 +0100

    To create a route, I used the get method on the app object.

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const express = require('express');
 //The express function returns an Express application.
 const app = express();
 
+//To create a route, I used the get method on the app object.
+//The get method is used to handle the get requests to a certain URL.
+app.get('/', (request, response) => {
+	response.send('Flashcards');
+});
+
 //Setup the development server using the listen method. 
 //I give this one parameter which is the port number 3000.
 app.listen(3000);
