b222112ab4b1a965ecc898ff71a868f6db6fe665
Mariusz <mariusz.kkk@poczta.fm>
2019-01-30 20:50:46 +0100

    I use the variable express to access all the methods and properties of the Express module

---
+++ app.js
@@ -0,0 +1,4 @@
+/*The module is called Express and it is the parameter that I pass into the require function. 
+  I can use the variable express to access all the methods and properties of the Express module.
+*/
+const express = require('express');
