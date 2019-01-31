667608f380b9f94e9b6038c4a63878437779b985
Mariusz <mariusz.kkk@poczta.fm>
2019-01-31 01:56:04 +0100

    Set the view engine, to the parameter pug

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const express = require('express');
 //The express function returns an Express application.
 const app = express();
 
+//Using the app.set method to set the view engine, To the parameter pug.
+//This line, just tells Express which template engine to use.
+app.set('view engine', 'pug');
+
 //To create a route, I used the get method on the app object.
 //The get method is used to handle the get requests to a certain URL.
 app.get('/', (request, response) => {
@@ -13,5 +17,7 @@ app.get('/', (request, response) => {
 });
 
 //Setup the development server using the listen method. 
-//I give this one parameter which is the port number 3000.
-app.listen(3000);
+//I give this parameter which is the port number 3000.
+app.listen(3000, () => {
+	console.log("The application is working on localhost:3000");
+});
