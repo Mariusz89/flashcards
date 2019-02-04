a93a5b000cc23055532b4a82270dec9f5e2de899
Mariusz <mariusz.kkk@poczta.fm>
2019-02-05 00:31:24 +0100

    Declared all routes with the router instead of the app.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+//With Express I'll use the router constructor to create a new router.
 const router = express.Router();
 
 
 //To create a route, I used the get method on the app object.
 //The get method is used to handle the get requests to a certain URL.
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
 	const name = req.cookies.username;
 	//I made an if statement checking the value of name.
 	//If it exists, we want to render the index template, otherwise redirect to the hello route.
@@ -16,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 //Rendering the hello template.
-app.get('/hello', (req, res) => {
+router.get('/hello', (req, res) => {
 	const name = req.cookies.username;
 	//Redirect the user to the index page if the cookies username value is set. 
 	//Otherwise, we went to render the hello form.
@@ -27,20 +28,21 @@ app.get('/hello', (req, res) => {
 	}
 });
 
-app.post('/hello', (req, res) => {
+router.post('/hello', (req, res) => {
 	//This will send a cookie to the browser after we submit the form.
 	res.cookie('username', req.body.username);
 	res.redirect('/');
 });
 
 //Clear the cookie in the new route, then redirect the user to the hello form.
-app.post('/goodbye', (req, res) => {
+router.post('/goodbye', (req, res) => {
 	res.clearCookie('username');
 	res.redirect('/hello');
 });
 
 //This will then serve the cards for the app.
 //prompt,  hint are the names for variables we want the view to have access to when it's being rendered.
-app.get('/card', (req, res) => {
+router.get('/card', (req, res) => {
 	res.render('card', {prompt: "Who is buried in Grant´s tomb?", hint: "Think about whose tomb it is"});
 });
+
