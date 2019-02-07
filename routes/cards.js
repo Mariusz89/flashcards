c60463f7b7ade0440c9bd12bac104d04bea21fff
Mariusz <mariusz.kkk@poczta.fm>
2019-02-07 19:12:47 +0100

    Wrap the text and the hint into an object that I can pass into the template.

--- routes/cards.js
+++ routes/cards.js
@@ -9,13 +9,14 @@ const { cards } = data;
 //Prompt, hint are the names for variables we want the view to have access to when it's being rendered.
 //I use a colon to tell express the treat this part of the URL as a variable or a route parameter named id.
 router.get('/:id', (req, res) => {
+	//The value for the route parameter from the URL will be stored in the request object params property.
 	const { side } = req.query;
+	//I use the ID parameter to access the elements in the card's array.
 	const { id } = req.params;
-	res.render('card', {
-		//The value for the route parameter from the URL will be stored in the request object params property.
-		//I use the ID parameter to access the elements in the card's array.
-		prompt: cards[req.params.id].question, 
-		hint: cards[req.params.id].hint});
-});
+	const text = cards[id][side];
+	const { hint } = cards[id];
+	//Wrap the text and the hint into an object that I can pass into the template.
+	const templateData = { text, hint};
+	res.render('card', templateData);
 
 module.exports = router;
