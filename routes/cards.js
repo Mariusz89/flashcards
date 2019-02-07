ae3701ae07af5efd360b4fed2f185e56070525c3
Mariusz <mariusz.kkk@poczta.fm>
2019-02-07 23:09:04 +0100

    Plug randomCard into the URL and it's used to move to a different page.

--- routes/cards.js
+++ routes/cards.js
@@ -5,6 +5,15 @@ const { data } = require('../data/flashcardData.json');
 //I've separated out the cards because the cards will be the main data we will want to use.
 const { cards } = data;
 
+router.get('/', (req, res) => {
+	//Total number of cards we have to choose from.
+	const lengthCards = cards.length;
+	//Generated a random card using math.random.
+	const randomCards = Math.floor( Math.random() * lengthCards);
+	//Plug that number into the URL and it's used to move to a different page.
+	res.redirect(`/cards/${randomCards}?side=question`);
+});
+
 //This will then serve the cards for the app.
 //Prompt, hint are the names for variables we want the view to have access to when it's being rendered.
 //I use a colon to tell express the treat this part of the URL as a variable or a route parameter named id.
@@ -16,8 +25,9 @@ router.get('/:id', (req, res) => {
 	const text = cards[id][side];
 	const { hint } = cards[id];
 	//Wrap the text and the hint into an object that I can pass into the template.
-	const templateData = { text, hint, side};
+	const templateData = { text, hint, side, id };
 	res.render('card', templateData);
 });
 
 module.exports = router;
+
