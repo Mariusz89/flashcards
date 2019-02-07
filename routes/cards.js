314d79a31781f224b65764227015d17fe98dfb9a
Mariusz <mariusz.kkk@poczta.fm>
2019-02-07 18:57:48 +0100

    Created variable side and id.First variable has property question and answer on the json. Second variable hold the ID from the route parameter

--- routes/cards.js
+++ routes/cards.js
@@ -9,9 +9,11 @@ const { cards } = data;
 //Prompt, hint are the names for variables we want the view to have access to when it's being rendered.
 //I use a colon to tell express the treat this part of the URL as a variable or a route parameter named id.
 router.get('/:id', (req, res) => {
+	const { side } = req.query;
+	const { id } = req.params;
 	res.render('card', {
 		//The value for the route parameter from the URL will be stored in the request object params property.
-		//I use the ID parameter to access the elements in the card's array,
+		//I use the ID parameter to access the elements in the card's array.
 		prompt: cards[req.params.id].question, 
 		hint: cards[req.params.id].hint});
 });
